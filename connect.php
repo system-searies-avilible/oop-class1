aee51e06024558c95fe85cd02f510ae6f367aa88
system-searies-avilible <111744571+system-searies-avilible@users.noreply.github.com>
2022-09-21 11:33:08 +0400

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+include"cv.php";
+$obj=new("cv");
+$obj-> per_info("Moheez","Abdul-Razzaq","1999-8-9","42301-10000-7","Islam","Pakistan");
+
+
+?>
---
+++ cv.php
@@ -0,0 +1,18 @@
+<?php
+class cv
+{
+function per_info($name,$fname,$db,$cnic,$relishion,$nationality){
+	echo"<h1> Personal information</h1>";
+echo"Name :".$name."<br>";
+echo"Father Name :".$fname."<br>";
+echo"Date of birth :".$db."<br>";
+echo"Cnic :".$cnic."<br>";
+echo"Relishion :".$relishion."<br>";
+echo"Nationality :".$nationality."<br>";
+
+}	
+	
+}
+
+
+?>
